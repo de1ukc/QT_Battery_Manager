0b4b391cf22f337106261c9c173720a40d9374ba
Aleh <Trapper.MD@mail.ru>
2022-10-05 16:03:50 +0300

    Add report

---
+++ BatteryManager/batterymanager.cpp
@@ -0,0 +1,7 @@
+#include "batterymanager.h"
+
+
+batteryManager::batteryManager()
+{
+
+}
---
+++ BatteryManager/batterymanager.h
@@ -0,0 +1,11 @@
+#ifndef BATTERYMANAGER_H
+#define BATTERYMANAGER_H
+
+
+class batteryManager
+{
+public:
+    batteryManager();
+};
+
+#endif // BATTERYMANAGER_H
